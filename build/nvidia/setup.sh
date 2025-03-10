7330b3e5ef99c093017b931b4626e1a362686d86
Shiba <44804845+DeepDoge@users.noreply.github.com>
2025-03-10 17:40:48 +0300

    puff

--- build/nvidia/setup.sh
+++ build/nvidia/setup.sh
@@ -12,4 +12,4 @@ FEDORA_VERSION=$(rpm -E %fedora)
 wget https://copr.fedorainfracloud.org/coprs/lukenukem/asus-linux/repo/fedora-${FEDORA_VERSION}/lukenukem-asus-linux-fedora-${FEDORA_VERSION}.repo -O /etc/yum.repos.d/_copr_lukenukem-asus-linux.repo
 
 # These should be more than enough.
-rpm-ostree install --idempotent akmod-nvidia xorg-x11-drv-nvidia{,-cuda} supergfxctl
+rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia{,-cuda} supergfxctl
